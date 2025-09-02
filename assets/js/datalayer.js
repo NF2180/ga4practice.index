d29c50196a7bfc68ac611d5169f3cf169ab02d49
NF2180 <nitin.fernandes30@gmail.com>
2025-09-02 13:33:02 +0530

    Add files via upload

--- assets/js/datalayer.js
+++ assets/js/datalayer.js
@@ -1,6 +1,8 @@
 
+// datalayer.js
 window.dataLayer = window.dataLayer || [];
-function pushEvent(name,params){
-  dataLayer.push(Object.assign({event:name},params||{}));
-  console.log('Pushed',name,params);
+
+function pushEvent(name, params) {
+  window.dataLayer.push(Object.assign({ event: name }, params || {}));
+  console.log("Pushed event:", name, params || {});
 }
