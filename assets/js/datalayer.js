6fa151b9c9e0f5b6d32ca0009f97573a6d3c7ccc
NF2180 <nitin.fernandes30@gmail.com>
2025-09-02 16:48:20 +0530

    Delete assets/js/datalayer.js and delete old files

--- assets/js/datalayer.js
+++
@@ -1,8 +0,0 @@
-
-// datalayer.js
-window.dataLayer = window.dataLayer || [];
-
-function pushEvent(name, params) {
-  window.dataLayer.push(Object.assign({ event: name }, params || {}));
-  console.log("Pushed event:", name, params || {});
-}
